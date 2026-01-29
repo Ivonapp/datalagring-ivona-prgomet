db03305fb86bb1e19d209c1204fa74c8d407d8d3
Ivonapp <ivona.v.hellsten@hotmail.com>
2026-01-29 23:08:28 +0100

    Working on my InfrastructureDbContext.

--- LearningPlatform.Infrastructure/Data/InfrastructureDbContext.cs
+++ LearningPlatform.Infrastructure/Data/InfrastructureDbContext.cs
@@ -13,6 +13,9 @@ public sealed class InfrastructureDbContext(DbContextOptions<InfrastructureDbCon
     public DbSet<ParticipantEntity> Participants { get; set; }
     public DbSet<TeacherEntity> Teachers { get; set; }
     
+
+    //hHär nedan definierar vi HUR strukturen ska se ut i självaste DATABASEN: (Hans video 24:46 Använd kodbaserad modell för SQL-databas med Entity Framework Core (Code first)
+    //Koden ovanför med alla public DbSet ska sen tas bort när nedan är färdig, ovan är bara en mall atm.
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
