93779dcecacb69b05d9ea5a978b4ed4c63ec84d4
Ivonapp <ivona.v.hellsten@hotmail.com>
2026-01-30 03:27:23 +0100

    Still working on InfrastructureDbContext. Made smaller updates on email. Have not yet added dependency/beroende.

--- LearningPlatform.Infrastructure/Data/InfrastructureDbContext.cs
+++ LearningPlatform.Infrastructure/Data/InfrastructureDbContext.cs
@@ -1,25 +1,24 @@
 ﻿using LearningPlatform.Infrastructure.Models;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace LearningPlatform.Infrastructure.Data;
 
-//PÅGÅENDE KOD. Denna är inte färdig. Försöker få ihop nån sorts struktur som jag förstår.
 public sealed class InfrastructureDbContext(DbContextOptions<InfrastructureDbContext> options) : DbContext(options)
 {
    
 
     //OnModelCreating = bestämmer hur tabellerna ska se ut i databasen
-    //hHär nedan definierar vi HUR strukturen ska se ut i självaste DATABASEN: (Hans video 24:46 Använd kodbaserad modell för SQL-databas med Entity Framework Core (Code first)
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
 
 
 
                     // COURSE 
-
-                    modelBuilder.Entity<CourseEntity>(entity =>                         // COURSEENTITY
+                    modelBuilder.Entity<CourseEntity>(entity =>                         // COURSE
                 {
-                    entity.ToTable("CourseEntity");                                     // KLASSNAMN. namnet inuti ("Course") kommer bli namnet inuti databasen.
+                    entity.ToTable("Course");                                           // KLASSNAMN. namnet inuti ("Course") kommer bli namnet inuti databasen.
                     entity.HasKey(e => e.Id).HasName("Pk_Course_Id");                   // PRIMARY KEY
 
                     entity.Property(e => e.Concurrency)                                 // SÄKERHET
@@ -41,12 +40,10 @@ public sealed class InfrastructureDbContext(DbContextOptions<InfrastructureDbCon
 
 
                     // COURSE SESSION
-
-                    modelBuilder.Entity<CourseSessionEntity>(entity =>                 // COURSESESSIONENTITY
+                    modelBuilder.Entity<CourseSessionEntity>(entity =>                 // COURSESESSION
                 {
-           
-                    entity.ToTable("CourseSessionEntity");                             //< KLASSNAMN. namnet inuti ("Course") kommer bli namnet inuti databasen.
-                    entity.HasKey(e => e.Id).HasName("Pk_CourseSession_Id");           //PRIMARY KEY
+                    entity.ToTable("CourseSession");                                   // KLASSNAMN. namnet inuti ("Course") kommer bli namnet inuti databasen.
+                    entity.HasKey(e => e.Id).HasName("Pk_CourseSession_Id");           // PRIMARY KEY
 
                     entity.Property(e => e.CourseId)
                    .IsRequired();
@@ -56,10 +53,10 @@ public sealed class InfrastructureDbContext(DbContextOptions<InfrastructureDbCon
                     .IsConcurrencyToken()                                               // SÄKERHET
                     .IsRequired();                                                      // SÄKERHET
 
-                    entity.Property(e => e.StartDate)                                   //START DATUM
+                    entity.Property(e => e.StartDate)                                   // START DATUM
                     .IsRequired();
 
-                    entity.Property(e => e.EndDate)                                     //SLUT DATUM
+                    entity.Property(e => e.EndDate)                                     // SLUT DATUM
                     .IsRequired();
 
                 });
@@ -68,19 +65,17 @@ public sealed class InfrastructureDbContext(DbContextOptions<InfrastructureDbCon
 
 
                     // ENROLLMENT
-
-                    modelBuilder.Entity<EnrollmentEntity>(entity =>                     //ENROLLMENTENTITY
+                    modelBuilder.Entity<EnrollmentEntity>(entity =>                     // ENROLLMENT
                 {
-
-                    entity.ToTable("EnrollmentEntity");                                 //KLASSNAMN. namnet inuti ("Course") kommer bli namnet inuti databasen.
-                    entity.HasKey(e => e.Id).HasName("Pk_Enrollment_Id");               //PRIMARY KEY
+                    entity.ToTable("Enrollment");                                       // KLASSNAMN. namnet inuti ("Course") kommer bli namnet inuti databasen.
+                    entity.HasKey(e => e.Id).HasName("Pk_Enrollment_Id");               // PRIMARY KEY
 
                     entity.Property(e => e.Concurrency)                                 // SÄKERHET
                     .IsRowVersion()                                                     // SÄKERHET
                     .IsConcurrencyToken()                                               // SÄKERHET
                     .IsRequired();                                                      // SÄKERHET
 
-                    entity.Property(e => e.EnrollmentDate)                              //ENROLLMENTDATE
+                    entity.Property(e => e.EnrollmentDate)                              // ENROLLMENTDATE
                     .IsRequired();
 
                 });
@@ -88,70 +83,76 @@ public sealed class InfrastructureDbContext(DbContextOptions<InfrastructureDbCon
 
 
                     //PARTICIPANT
-
-                    modelBuilder.Entity<ParticipantEntity>(entity =>                    //PARTICIPANTENTITY
+                    modelBuilder.Entity<ParticipantEntity>(entity =>                    // PARTICIPANT
                 {
-                    entity.ToTable("ParticipantEntity");                                //KLASSNAMN. namnet inuti ("Course") kommer bli namnet inuti databasen.
-                    entity.HasKey(e => e.Id).HasName("Pk_Participant_Id");              //PRIMARY KEY
+                    entity.ToTable("Participant");                                      // KLASSNAMN. namnet inuti ("Course") kommer bli namnet inuti databasen.
+                    entity.HasKey(e => e.Id).HasName("Pk_Participant_Id");              // PRIMARY KEY
 
                     entity.Property(e => e.Concurrency)                                 // SÄKERHET
                      .IsRowVersion()                                                    // SÄKERHET
                      .IsConcurrencyToken()                                              // SÄKERHET
                      .IsRequired();                                                     // SÄKERHET
 
-                    entity.Property(e => e.FirstName)                                   //FÖRNAMN
-                    .HasMaxLength(100)                                                  //MAX ANTAL TECKEN FÖR FÖRNAMN
-                    .IsRequired();                                                      //FÖRNAMN ÄR OBLIGATORISKT
+                    entity.Property(e => e.FirstName)                                   // FÖRNAMN
+                    .HasMaxLength(100)                                                  // MAX ANTAL TECKEN FÖR FÖRNAMN
+                    .IsRequired();                                                      // FÖRNAMN ÄR OBLIGATORISKT
 
-                    entity.Property(e => e.LastName)                                    //EFTERNAMN
-                    .HasMaxLength(100)                                                  //MAX ANTAL TECKEN FÖR EFTERNAMN
-                    .IsRequired();                                                      //EFTERNAMN ÄR OBLIGATORISKT
+                    entity.Property(e => e.LastName)                                    // EFTERNAMN
+                    .HasMaxLength(100)                                                  // MAX ANTAL TECKEN FÖR EFTERNAMN
+                    .IsRequired();                                                      // EFTERNAMN ÄR OBLIGATORISKT
 
-                    entity.Property(e => e.Email)                                       //EMAIL
-                    .HasMaxLength(250)                                                  //MAX ANTAL TECKEN FÖR EMAIL
-                    .IsRequired();                                                      //EMAIL ÄR OBLIGATORISKT
+                    entity.Property(e => e.Email)                                       // EMAIL
+                    .HasMaxLength(250)                                                  // MAX ANTAL TECKEN FÖR EMAIL
+                    .IsRequired();                                                      // EMAIL ÄR OBLIGATORISKT
 
-                    entity.Property(e => e.PhoneNumber)                                 //PHONENUMBER
-                    .HasMaxLength(15)                                                   //MAX ANTAL TECKEN FÖR NUMMER
-                    .IsUnicode(false)                                                   //TAR BORT SPECIALTECKEN
-                    .IsRequired();                                                      //TELEFONNUMMER ÄR OBLIGATORISKT
+                    entity.HasIndex(e => e.Email)
+                    .IsUnique();
+
+                    entity.Property(e => e.PhoneNumber)                                 // PHONENUMBER
+                    .HasMaxLength(15)                                                   // MAX ANTAL TECKEN FÖR NUMMER
+                    .IsUnicode(false)                                                   // TAR BORT SPECIALTECKEN
+                    .IsRequired();                                                      // TELEFONNUMMER ÄR OBLIGATORISKT
                 });
 
 
 
 
                     //TEACHER
-                    modelBuilder.Entity<TeacherEntity>(entity =>                        // TEACHERENTITY
+                    modelBuilder.Entity<TeacherEntity>(entity =>                        // TEACHER
                 {
-                    entity.ToTable("TeacherEntity");                                    //KLASSNAMN. namnet inuti ("Course") kommer bli namnet inuti databasen.
-                    entity.HasKey(e => e.Id).HasName("Pk_Teacher_Id");                  //PRIMARY KEY
+                    entity.ToTable("Teacher");                                          // KLASSNAMN. namnet inuti ("Course") kommer bli namnet inuti databasen.
+                    entity.HasKey(e => e.Id).HasName("Pk_Teacher_Id");                  // PRIMARY KEY
 
                     entity.Property(e => e.Concurrency)                                 // SÄKERHET
                     .IsRowVersion()                                                     // SÄKERHET
                     .IsConcurrencyToken()                                               // SÄKERHET
                     .IsRequired();                                                      // SÄKERHET
 
-                    entity.Property(e => e.FirstName)                                   //FÖRNAMN
-                    .HasMaxLength(100)                                                  //MAX ANTAL TECKEN FÖR FÖRNAMN
-                    .IsRequired();                                                      //FÖRNAMN ÄR OBLIGATORISKT
+                    entity.Property(e => e.FirstName)                                   // FÖRNAMN
+                    .HasMaxLength(100)                                                  // MAX ANTAL TECKEN FÖR FÖRNAMN
+                    .IsRequired();                                                      // FÖRNAMN ÄR OBLIGATORISKT
 
-                    entity.Property(e => e.LastName)                                    //EFTERNAMN
-                    .HasMaxLength(100)                                                  //MAX ANTAL TECKEN FÖR EFTERNAMN
-                    .IsRequired();                                                      //EFTERNAMN ÄR OBLIGATORISKT
+                    entity.Property(e => e.LastName)                                    // EFTERNAMN
+                    .HasMaxLength(100)                                                  // MAX ANTAL TECKEN FÖR EFTERNAMN
+                    .IsRequired();                                                      // EFTERNAMN ÄR OBLIGATORISKT
 
-                    entity.Property(e => e.Email)                                       //EMAIL
-                    .HasMaxLength(250)                                                  //MAX ANTAL TECKEN FÖR EMAIL
-                    .IsRequired();                                                      //EMAIL ÄR OBLIGATORISKT
+                    entity.Property(e => e.Email)                                       // EMAIL
+                    .HasMaxLength(250)                                                  // MAX ANTAL TECKEN FÖR EMAIL
+                    .IsRequired();                                                      // EMAIL ÄR OBLIGATORISKT
 
-                    entity.Property(e => e.PhoneNumber)                                 //PHONENUMBER
-                    .HasMaxLength(15)                                                   //MAX ANTAL TECKEN FÖR NUMMER
-                    .IsUnicode(false)                                                   //TAR BORT SPECIALTECKEN
-                    .IsRequired();                                                      //TELEFONNUMMER ÄR OBLIGATORISKT
+                    entity.HasIndex(e => e.Email)
+                    .IsUnique();
 
-                    entity.Property(e => e.Major)                                       //PHONENUMBER
-                    .HasMaxLength(15)                                                   //MAX ANTAL TECKEN FÖR NUMMER
-                    .IsRequired();                                                      //TELEFONNUMMER ÄR OBLIGATORISKT
+                    entity.Property(e => e.PhoneNumber)                                 // PHONENUMBER
+                    .HasMaxLength(15)                                                   // MAX ANTAL TECKEN FÖR NUMMER
+                    .IsUnicode(false)                                                   // TAR BORT SPECIALTECKEN
+                    .IsRequired();                                                      // TELEFONNUMMER ÄR OBLIGATORISKT
+
+                    entity.Property(e => e.Major)                                       // MAJOR / SKOLÄMNE
+                    .HasMaxLength(100)                                                  // MAX ANTAL TECKEN FÖR ÄMNE
+                    .IsRequired();                                                      // ÄMNE ÄR OBLIGATORISKT
                 });
+
 
 
 
