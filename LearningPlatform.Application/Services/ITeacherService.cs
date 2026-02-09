3b25b4d56601932120c7d1a6ea35f6ccdd296930
Ivonapp <ivona.v.hellsten@hotmail.com>
2026-02-09 16:37:56 +0100

    Building the Service. Starting with TeacherService and ITeacherService.

--- LearningPlatform.Application/Services/ITeacherService.cs
+++ LearningPlatform.Application/Services/ITeacherService.cs
@@ -1,41 +1,43 @@
 ﻿using LearningPlatform.Application.Teachers.PersistenceModels;
+using Microsoft.VisualBasic;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace LearningPlatform.Application.Services;
 
 public interface ITeacherService
 {
+
+    //OMBANDLAR HANS KOD SÅ DEN PASSAR I MTT PROJEKT OCH FÖLJER CRUD - INTE SÄKER PÅ OM DETTA STÄMMER !!
+
+    // C - Create (Skapa)
+    Task CreateAsync(TeacherModel model, CancellationToken ct = default);
+
+    // R - Read (Hämta en)
+    Task<TeacherModel?> GetByIdAsync(int id, CancellationToken ct = default);
+
+    // R - Read (Hämta alla)
+    Task<IReadOnlyList<TeacherModel>> ListAsync(CancellationToken ct = default);
+
+    // U - Update (Uppdatera)
+    Task UpdateAsync(TeacherModel model, CancellationToken ct = default);
+
+    // D - Delete (Ta bort)
+    Task DeleteAsync(int id, CancellationToken ct = default);
+
 }
 
 
 
 
 
-// ANVÄND DIG AV KODEN I DESSA FÖR ATT SKAPA INTERFACET FÖR SERVICE
+        // ** HANS KOD **
+        //Task<Guid> CreateAsync(CreateUserInput input, CancellationToken ct); //skapade en input i persistence i INPUTS
 
-// IRepositoryBase
+        //Task<UserOutput?> GetByIdAsync(Guid userId, CancellationToken ct); //skapade en UserOutput i OUTPUTS 
 
+        //Task<IReadOnlyList<UserOutput>> ListAsync(CancellationToken ct = default);
 
-
-
-
-
-
-
-// TeacherModel
-
-
-
-
-
-
-
-
-
-
-
-// ITeacherRepository
-
-
+        //Task DeleteAsync(Guid userId, byte[] rowVersion, CancellationToken ct = default);
