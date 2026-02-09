2a54e1773696587213a3027cec45cb95213e4387
Ivonapp <ivona.v.hellsten@hotmail.com>
2026-02-09 14:46:54 +0100

    Finished the last repository, CourseRepository. The code might have some minor errors and issues, but for now I'll proceed to working on the Service.

--- LearningPlatform.Infrastructure/EFC/Repositories/CourseRepository.cs
+++ LearningPlatform.Infrastructure/EFC/Repositories/CourseRepository.cs
@@ -2,6 +2,7 @@
 using LearningPlatform.Application.Courses.PersistenceModels;
 using LearningPlatform.Infrastructure.EFC.Data;
 using LearningPlatform.Infrastructure.EFC.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -13,19 +14,58 @@ namespace LearningPlatform.Infrastructure.EFC.Repositories
 {
     public class CourseRepository(InfrastructureDbContext Context) : EfcRepositoryBase<CourseEntity, int, CourseModel>(Context), ICourseRepository
     {
-        public override Task AddAsync(CourseModel model, CancellationToken ct = default)
+
+
+        //TOMODEL
+        //FÖLJ MALLEN I MODEL
+                public override CourseModel ToModel(CourseEntity entity) => new(
+                entity.Id,
+                entity.CourseCode,
+                entity.Concurrency,
+                entity.Title,
+                entity.Description,
+                entity.CreatedAt,
+                entity.UpdatedAt
+
+                );
+
+
+        //ADD
+        //INGEN ID, UPDATEDAT, CONCURRENCY
+        public override async Task AddAsync(CourseModel model, CancellationToken ct = default)
         {
-            throw new NotImplementedException();
+            var entity = new CourseEntity
+            {
+
+
+                CourseCode = model.CourseCode,
+                Title = model.Title,
+                Description = model.Description,
+                CreatedAt = DateTime.UtcNow
+
+            };
+
+            await Set.AddAsync(entity, ct);               // Lägger till i kön - (utan denna och koden nedan händer ingenting.)
         }
 
-        public override CourseModel ToModel(CourseEntity entity)
-        {
-            throw new NotImplementedException();
-        }
 
-        public override Task UpdateAsync(CourseModel model, CancellationToken ct = default)
-        {
-            throw new NotImplementedException();
+            //UPDATE
+
+            public override async Task UpdateAsync(CourseModel model, CancellationToken ct = default)
+            {
+                var entity = await Set.SingleOrDefaultAsync(x => x.Id == model.Id, ct)
+                ?? throw new ArgumentException($"Course {model.Id} not found."); //JUSTERA
+
+                Context.Entry(entity).Property(x => x.Concurrency).OriginalValue = model.Concurrency;
+
+
+                // 1. DATA SOM SKA GÅ ATT ÄNDRA!
+                entity.CourseCode = model.CourseCode;
+                entity.Title = model.Title;
+                entity.Description = model.Description;
+
+                // 2. TIDSTÄMPEL FÖR ÄNDRINGEN!
+                entity.UpdatedAt = DateTime.UtcNow;
         }
     }
 }
