21ddf3bebd7d907647885039dee3b9ffe73be640
Ivonapp <ivona.v.hellsten@hotmail.com>
2026-02-10 16:18:06 +0100

    Finished the last service, courseService.

--- LearningPlatform.Application/Courses/CourseService.cs
+++ LearningPlatform.Application/Courses/CourseService.cs
@@ -2,7 +2,10 @@
 using LearningPlatform.Application.Abstractions.Persistence.Repositories;
 using LearningPlatform.Application.Courses.Inputs;
 using LearningPlatform.Application.Courses.Outputs;
+using LearningPlatform.Application.Courses.PersistenceModels;
+using LearningPlatform.Application.Mappers;
 using LearningPlatform.Application.Services;
+using LearningPlatform.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,32 +15,75 @@ namespace LearningPlatform.Application.Courses;
 public sealed class CourseService
     (
     ICourseRepository course,
-    IUnitOfWork uowÖ
+    IUnitOfWork uow
     ) : ICourseService
 
 {
-    public Task CreateAsync(CourseInput input, CancellationToken ct = default)
+
+    // CREATE
+    public async Task<int> CreateAsync(CourseInput input, CancellationToken ct = default)
     {
-        throw new NotImplementedException();
+
+        var courseToCreate = new CourseModel(
+            0,
+            input.CourseCode,
+            Array.Empty<byte>(),
+            input.Title,
+            input.Description,
+            DateTime.UtcNow,
+            null
+        );
+
+        await course.AddAsync(courseToCreate, ct);
+        await uow.SaveChangesAsync(ct);
+
+        return courseToCreate.Id;
     }
 
-    public Task DeleteAsync(int id, CancellationToken ct = default)
+
+    // DELETE
+    public async Task DeleteAsync(int id, CancellationToken ct = default)
     {
-        throw new NotImplementedException();
+        var existing = await course.GetByIdAsync(id, ct)
+            ?? throw new ArgumentException($"Course {id} not found.");
+
+        await course.UpdateAsync(existing, ct);
+        await course.DeleteAsync(id, ct);
+        await uow.SaveChangesAsync(ct);
     }
 
-    public Task<CourseOutput?> GetByIdAsync(int id, CancellationToken ct = default)
+    // GET BY ID
+    public async Task<CourseOutput?> GetByIdAsync(int id, CancellationToken ct = default)
     {
-        throw new NotImplementedException();
+        var model = await course.GetByIdAsync(id, ct);
+        return model is null ? null : CourseMapper.ToOutput(model);
     }
 
-    public Task<IReadOnlyList<CourseOutput>> ListAsync(CancellationToken ct = default)
+
+
+    // LIST
+    public async Task<IReadOnlyList<CourseOutput>> ListAsync(CancellationToken ct = default)
     {
-        throw new NotImplementedException();
+        var models = await course.ListAsync(ct);
+        return CourseMapper.ToOutputList(models);
     }
 
-    public Task UpdateAsync(int id, CourseInput input, CancellationToken ct = default)
+
+    // UPDATE
+    public async Task UpdateAsync(int id, CourseInput input, CancellationToken ct = default)
     {
-        throw new NotImplementedException();
+        var existing = await course.GetByIdAsync(id, ct)
+            ?? throw new ArgumentException($"Course {id} not found.");
+
+        var model = CourseMapper.ToModel(input) with
+        {
+            Id = id,
+            Concurrency = existing.Concurrency,
+            UpdatedAt = DateTime.UtcNow
+        };
+
+        await course.UpdateAsync(model, ct);
+        await uow.SaveChangesAsync(ct);
+
     }
 }
